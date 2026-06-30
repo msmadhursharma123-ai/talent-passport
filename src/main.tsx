31cde0a7253017d3c002c22fbaa2518ea17a2929
Madhur Sharma <msmadhursharma123@gmail.com>
2026-06-30 15:05:54 +0530

    Fix CSS import paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import './styles/theme.css'
-import "./Styles/landingImports.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import "./index.css";
+import "./styles/theme.css";
+import "./styles/LandingImports.css";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 );
